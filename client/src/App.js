8bacceb315965afc3a8b7b5ac99693e5db4e41ff
suyoung <dltnk9@naver.com>
2022-10-06 17:20:01 +1100

    Update Frontend

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul> */}
+
+        <hr />
+
+        <Routes>
+          <Route exact path="/" element = {<LandingPage />} />
+          <Route exact path="/login" element = {<LoginPage />} />
+          <Route exact path="/register" element = {<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+
+export default App;
+
